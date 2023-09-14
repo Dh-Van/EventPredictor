d774b3643ca1e23819645ff4bdaeec2932c739e8
Dh-Van <dhvan17shah@gmail.com>
2023-09-14 14:32:46 +0000

    Showing just in season events

--- Team.py
+++ Team.py
@@ -6,17 +6,27 @@ class TeamData():
 
     def __init__(self, team):
         self.tba = TBA()
-
-        event_data = self.tba.get_data("/team/frc" + team + "/events")
-        for event in event_data:
-            week = event["week"]
-            if(week is not None) and int(event["year"]) >= 2020:
-                key = event["key"]
-                lat = event["lat"]
-                lng = event["lng"]
-                self.events.append([key, week, [lat, lng]])
+        self.team = team
+        self.events = self.get_events()
         print(self.events)
 
+        
+
+    def get_events(self):
+        event_data = self.tba.get_data("/team/frc" + self.team + "/events")
+        events = []
+        for event in event_data:
+            week = event["week"]
+            year = int(event["year"])
+            if(event["event_type"] <= 5 and year >= 2020 and year != 2021):
+                lat = float(event["lat"])
+                lng = float(event["lng"])
+                if(week == None): week = 7
+                events.append([year, int(week) + 1, [lat, lng]])
+        
+        events.sort(key=lambda x:(x[0], x[1]))
+        
+        return events
 
 T4909 = TeamData("4909")
 
